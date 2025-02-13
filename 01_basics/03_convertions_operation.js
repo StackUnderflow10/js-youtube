a35a28394cb366e2ead16d10741454330d8a88f3
StackUnderflow10 <imsarkarjeet012006@gmail.com>
2025-02-13 13:57:58 +0000

    comparison_04

--- 01_basics/03_convertions_operation.js
+++ 01_basics/03_convertions_operation.js
@@ -16,8 +16,36 @@
 // ""=>false
 // "jeet"=>true
 
-let someNumber=33
+// let someNumber=33
 
-let stringnumber=String(someNumber)
-console.log(stringnumber)
-console.log(typeof(stringnumber))
+// let stringnumber=String(someNumber)
+// console.log(stringnumber)
+// console.log(typeof(stringnumber))
+
+let value= 3;
+let negValue=-value
+console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+
+
+let str1="hello"
+let str2="world"
+
+
+let str3= str1+str2
+console.log(str3);
+
+console.log("1"+2+2);
+console.log(2+2+"1");
+
+let num1,num2,num3;
+
+num1=num2=num3=2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
