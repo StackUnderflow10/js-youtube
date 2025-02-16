2b96ef008ca4056ca10a632fee4534d49571854d
StackUnderflow10 <imsarkarjeet012006@gmail.com>
2025-02-16 06:41:17 +0000

    datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,26 @@
+// primitive
+
+// 7 types: String,number,boolean,null,undefined,symbol,bigint
+
+// const id =Symbol('123')
+// const anotherId=Symbol('123')
+
+// console.log(id===anotherId);
+
+
+
+// reference (non primitive)
+
+// array,objects,functions
+
+// const heroes = ["batman","ironman","spidey"]
+// let mybio={
+//     name:"jeet",
+//     age:"2929"
+// }
+
+const myFunction=function(){
+    console.log("hello world");
+
+}
+console.log(typeof(myFunction));
