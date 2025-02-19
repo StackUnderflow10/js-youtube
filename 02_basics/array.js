12f23f8805a9d4abf1118c89d46e4a47f2feff4d
StackUnderflow10 <imsarkarjeet012006@gmail.com>
2025-02-19 16:11:08 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,35 @@
+// array
+
+const myArray = [0,1,2,3,4,5]
+const myHeros = ["spidey","batman"]
+const myArr2= new Array(1,2,3,4,5)
+console.log(myArray[1]);
+
+// array methods
+
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(9)
+// myArray.shift()
+
+// console.log(myArr2.includes(9));
+// console.log(myArr2.indexOf(3));
+// console.log(myArr2.indexOf(9));
+
+// const newArr = myArr2.join()
+// console.log(myArray);
+// console.log(newArr);
+// console.log(typeof newArr);
+console.log("A",myArray);
+const myn1 = myArray.slice(1,3);
+console.log(myn1);
+console.log("B",myArray);
+const myn2 = myArray.splice(1,3)
+console.log("C",myArray);
+console.log(myn2);
+
+
+
