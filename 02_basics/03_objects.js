81c76b821e91f35947d7345d8eba9e59438601f8
StackUnderflow10 <imsarkarjeet012006@gmail.com>
2025-03-03 11:57:38 +0000

    array and objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,42 @@
+// singleton 
+
+// object literals 
+const mySym=Symbol("key1")
+const JsUser = {
+    name : "jeet",
+    "full name" : "jeet sarkar",
+    age : 19,
+    location: "kolkata",
+    email: "jeet@ping.com",
+    isLoggedIn : false,
+    lastLoginDays: ["Monday","Saturday"],
+    [mySym] : "newkey1"
+
+
+
+    
+
+
+}
+// console.log(JsUser["email"]);
+// console.log(JsUser.email);
+
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+// JsUser.email="jeet@yahoo.com"
+// console.log(JsUser["email"])
+
+
+JsUser.greeting = function()
+{
+    console.log("hello js user");
+}
+JsUser.greetingTwo = function()
+{
+    console.log('hello Js user, ${this.name}');
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+
